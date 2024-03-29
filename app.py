cee0ef1f6c68c94408f6381430f66df3e0d71806
krishnam <maheshwarikrishnam123@gmail.com>
2024-03-29 20:04:20 +0530

    FIX: cors issue

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, jsonify
-from data import *
+from flask_cors import CORS
+from data import get_questions 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
